a2dc797be861dc31feda6bd95393ad8da6baf1cd
OlenkaSav <94956727+OlenkaSav@users.noreply.github.com>
2022-05-18 16:20:05 +0300

    task2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery")
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}).join('');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+let gallery2 = new SimpleLightbox('.gallery a', {  captionDelay: 250, captionsData: "alt"});
+gallery2.on('show.simplelightbox', function () {
+
+});
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from "lodash.throttle";
+
+const loginForm = document.querySelector(".feedback-form");
+const emailForm = loginForm.elements.email;
+const messageForm = loginForm.elements.message;
+const submitBtn = loginForm.querySelector("button");
+let formData ={};
+
+loginForm.addEventListener("input", throttle(onFormInput, 500));
+loginForm.addEventListener('submit', onFormSubmit);
+
+
+onFormAutofill();
+
+function onFormSubmit(event){
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state");
+    console.log(formData);
+    formData={};
+  }
+
+function onFormInput(event){
+    formData[event.target.name]=event.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+}
+
+function onFormAutofill(){
+        const formSavedInfo =JSON.parse(localStorage.getItem('feedback-form-state'));
+    formData={...formSavedInfo}
+        if(formSavedInfo){
+        const {email="", message=""}=formSavedInfo;
+        emailForm.value=email;
+        messageForm.value=message;
+   
+    }
+
+};
 
